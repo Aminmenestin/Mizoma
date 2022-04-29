6d994a5d24315ae9533480fca1c939d05433e92d
Aminmenestin <96004789+Aminmenestin@users.noreply.github.com>
2022-04-29 22:12:09 +0430

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// header toggle
+const nav=document.querySelector('#nav');
+
+function open_sidebar(){
+  nav.classList.add('active');
+}
+function close_sidebar(){
+  nav.classList.remove('active');
+}
+
+
+
